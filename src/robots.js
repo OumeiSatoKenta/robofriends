f6337cf82d5c512413b40ee7a3f7243b34f989e2
OumeiSatoKenta <satoken.sk.722@gmail.com>
2022-07-22 07:38:48 +0900

    React

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Karen, Aijo',
+    username: 'Karen',
+    email: 'aijo@seisho.com',
+  },
+  {
+    id: 2,
+    name: 'Hikari, Kagura',
+    username: 'Hikari',
+    email: 'kagura@seisho.com',
+  },
+  {
+    id: 3,
+    name: 'Mhiru, Tuyusaki',
+    username: 'Mahiru',
+    email: 'tuyusaki@seisho.com',
+  },
+  {
+    id: 4,
+    name: 'Junna, Hoshimi',
+    username: 'Junna',
+    email: 'hoshimi@seisho.com',
+  },
+  {
+    id: 5,
+    name: 'Nana, Daiba',
+    username: 'Banana',
+    email: 'daiba@seisho.com',
+  },
+  {
+    id: 6,
+    name: 'Kaoruko, Hanayagi',
+    username: 'Kaoruko',
+    email: 'hanayagi@seisho.com',
+  },
+  {
+    id: 7,
+    name: 'Futaba, Isurugi',
+    username: 'Futaba',
+    email: 'isurugi@seisho.com',
+  },
+  {
+    id: 8,
+    name: 'Kuro, Saijo',
+    username: 'Kuro',
+    email: 'saijo@seisho.com',
+  },
+  {
+    id: 9,
+    name: 'Maya, Tendo',
+    username: 'Maya',
+    email: 'tendo@seisho.com',
+  },
+];
