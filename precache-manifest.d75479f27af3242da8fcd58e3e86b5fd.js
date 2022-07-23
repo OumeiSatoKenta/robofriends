4fc9b5544092ae684a3353df764ff0167a5223d9
OumeiSatoKenta <satoken.sk.722@gmail.com>
2022-07-23 10:11:48 +0900

    Updates

---
+++ precache-manifest.d75479f27af3242da8fcd58e3e86b5fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ed31c9ef23cd1d0ba10",
+    "url": "://OumeiSatoKenta.github.io/robofriends/static/css/main.837c225f.chunk.css"
+  },
+  {
+    "revision": "8ed31c9ef23cd1d0ba10",
+    "url": "://OumeiSatoKenta.github.io/robofriends/static/js/main.8ed31c9e.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "://OumeiSatoKenta.github.io/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "8d1e4cdaf4909ce3cb1a",
+    "url": "://OumeiSatoKenta.github.io/robofriends/static/css/2.83be0940.chunk.css"
+  },
+  {
+    "revision": "8d1e4cdaf4909ce3cb1a",
+    "url": "://OumeiSatoKenta.github.io/robofriends/static/js/2.8d1e4cda.chunk.js"
+  },
+  {
+    "revision": "38bd7bc65dc4b37c6f50",
+    "url": "://OumeiSatoKenta.github.io/robofriends/static/js/runtime~main.38bd7bc6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "://OumeiSatoKenta.github.io/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "352155905c4010453a411cbc611b120a",
+    "url": "://OumeiSatoKenta.github.io/robofriends/index.html"
+  }
+];
